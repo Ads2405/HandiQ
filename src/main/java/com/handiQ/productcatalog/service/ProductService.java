56f00c322032e43ff510090245d6ff0e1fbd84dc
Anmol Deshratna Saxena <anmolsaxena2405@gmail.com>
2025-08-17 13:34:41 +0530

    Added product API

---
+++ src/main/java/com/handiQ/productcatalog/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.handiQ.productcatalog.service;
+
+import com.handiQ.productcatalog.model.Product;
+import com.handiQ.productcatalog.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public List<Product> getProductByCategory(Long categoryId){
+        return productRepository.findByCategoryId(categoryId );
+    }
+}
